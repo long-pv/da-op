62dd50dbcfe91c922e52dbe0af2310cbc5911b6f
longpv <long.phamvan@adamosoft.com>
2025-05-05 16:45:47 +0700

    Clean up repo to match new .gitignore rules

--- wp-content/themes/xemer_theme/assets/js/menu.js
+++
@@ -1,9 +0,0 @@
-// burgermenu
-
-$('.menu-ham').click(function () {
-  $('.menu').animate({right: '0px'}, 100)
-});
-
-$('.close-menu').click(function () {
-  $('.menu').animate({right: '-400px'}, 100)
-});
