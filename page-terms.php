9325db68fc4669d1f2c38ab446aed540c0131bd2
Youngboy55 <phamvanvu411.nd@gmail.com>
2025-05-06 10:28:08 +0700

    Task: Term page

--- page-terms.php
+++ page-terms.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Template name: About Us
+ * Template name: Terms & Condition
  * The template for displaying all pages
  *
  * This is the template that displays all pages by default.
@@ -18,8 +18,51 @@ get_header();
 ?>
 
 <!-- Banner Start -->
-sec
+<?php
+$banner_terms = get_the_post_thumbnail_url(get_the_ID(), 'full');
+?>
+<style>
+    section.breadcrumb-outer:before {
+        background-image: url(<?php echo $banner_terms; ?>);
+        background-repeat: no-repeat;
+
+    }
+</style>
+
+<section class="breadcrumb-outer text-center">
+    <div class="container">
+        <div class="breadcrumb-content">
+            <h2 class="white"><?php the_title(); ?></h2>
+            <nav aria-label="breadcrumb">
+                <ul class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="<?php echo home_url(); ?>">Home</a></li>
+                    <li class="breadcrumb-item active" aria-current="page"><?php the_title(); ?></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+    <div class="overlay"></div>
+</section>
 <!-- Banner End -->
+
+<!-- Content Start -->
+<section class="terms">
+    <div class="container">
+        <div class="content">
+            <?php
+            $term_content = get_field('term_content') ?? [];
+            ?>
+            <?php if (isset($term_content) && is_array($term_content)): ?>
+                <?php foreach ($term_content as $term): ?>
+                    <div class="term editor">
+                        <?php echo $term['content']; ?>
+                    </div>
+                <?php endforeach; ?>
+            <?php endif; ?>
+        </div>
+    </div>
+</section>
+<!-- Content End -->
 
 <?php
 get_footer();
